73618357d62b38a11aee3d19a6d38fd7e7374728
Farie-p <43580712+Farie-p@users.noreply.github.com>
2024-02-05 00:57:50 +0200

    Update lesson_3.py

--- lesson_3.py
+++ lesson_3.py
@@ -30,8 +30,6 @@ for shamwari in shamwari:
 for index in range(5, 10):
     print(index)
 ############## EXPONENT FUNCTION #######################
-
-
 def raise_to_power(base_num, pow_num):
     result = 1
     for index in range(pow_num):
